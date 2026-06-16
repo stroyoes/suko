2591735b14101d12fbf71a464e7d1b04cfb9f748
stroyoes <mazky7804@gmail.com>
2026-06-16 15:36:54 +0530

    refactor(solver): rename board display function

--- solver.h
+++ solver.h
@@ -6,6 +6,6 @@
 bool solve_sudoku(int board[9][9]);
 bool is_valid_state(int board[9][9], int row, int col, int num);
 bool find_empty_cell(int board[9][9], int *row, int *col);
-void display_solved_sudoku(int board[9][9]);
+void display_sudoku(int board[9][9]);
 
 #endif 
