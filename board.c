d93394b5c25af487a5bc3aa69d9d4308957c5619
stroyoes <mazky7804@gmail.com>
2026-06-16 13:00:10 +0530

    feat(board): add hardcoded sudoku board

---
+++ board.c
@@ -0,0 +1,18 @@
+
+/*  Making sure the algorithm works. Matrix wont be hardcoded in later versions and therefore this file will be removed */ 
+
+#include "board.h"
+
+// NOTE: 0 means empty cell . Edit this to try different puzzles 
+
+int sudoku[9][9] = {
+    {5, 3, 0, 0, 7, 0, 0, 0, 0},
+    {6, 0, 0, 1, 9, 5, 0, 0, 0},
+    {0, 9, 8, 0, 0, 0, 0, 6, 0},
+    {8, 0, 0, 0, 6, 0, 0, 0, 3},
+    {4, 0, 0, 8, 0, 3, 0, 0, 1},
+    {7, 0, 0, 0, 2, 0, 0, 0, 6},
+    {0, 6, 0, 0, 0, 0, 2, 8, 0},
+    {0, 0, 0, 4, 1, 9, 0, 0, 5},
+    {0, 0, 0, 0, 8, 0, 0, 7, 9}
+};
---
+++ board.h
@@ -0,0 +1,7 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+extern int sudoku[9][9];
+
+#endif 
+
